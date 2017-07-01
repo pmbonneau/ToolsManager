e251aa446351d1d44b38449f742e183cd6875550
Pierre-Marc Bonneau <pierremarc_999@hotmail.com>
2017-07-01 14:58:13 -0400

    Added DmgUtility.

--- ToolsManagerLib/ToolsManagerLib/ToolsManager.cs
+++ ToolsManagerLib/ToolsManagerLib/ToolsManager.cs
@@ -7,7 +7,6 @@ using System.Diagnostics;
 
 namespace ToolsManagerLib
 {
-
     public class XPwnTools
     {
         public void DecryptFirmwareImage(string InputImagePath, string OutputImagePath, string DecryptionIV, string DecryptionKey)
@@ -27,6 +26,19 @@ namespace ToolsManagerLib
             startInfo.FileName = "idevicerestore.exe";
             startInfo.Arguments = "-e " + PathIPSW;
             Process.Start(startInfo);
+        }
+    }
+
+    public class DmgUtility
+    {
+        public void DecryptImage(string EncryptedInputDmgImagePath, string DecryptedOutputDmgImagePath, string DecryptionKey)
+        {
+            Process DecryptImage = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "dmg.exe";
+            startInfo.Arguments = "extract" + " " + EncryptedInputDmgImagePath + " " + DecryptedOutputDmgImagePath + " -k " + DecryptionKey;
+            DecryptImage = Process.Start(startInfo);
+            DecryptImage.WaitForExit();
         }
     }
 }
